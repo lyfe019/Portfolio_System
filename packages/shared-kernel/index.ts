cec23ecb1032963ab86349e56c31daedca8fc8a9
Giovanni Amanam Udoaka <gio2.udoaka@gmail.com>
2026-05-03 13:13:35 +0100

    feat(shared-kernel): implement shared utility types

--- packages/shared-kernel/index.ts
+++ packages/shared-kernel/index.ts
@@ -2,3 +2,4 @@ export * from "./src/errors";
 export * from "./src/events";
 export * from "./src/utilities";
 export * from "./src/timestamps";
+export * from "./src/types";
---
+++ packages/shared-kernel/src/types/index.ts
@@ -0,0 +1,5 @@
+export * from "./deep-partial";
+export * from "./maybe";
+export * from "./nullable";
+export * from "./optional";
+export * from "./primitive";
